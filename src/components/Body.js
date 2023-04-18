ec25c80ed7a1298faf32c4c17f010e49fcdbd603
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-04-18 19:06:34 +0200

    add block2

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,12 @@
 import arrow from "/home/friedrichtenhagen/ecommerce-site/src/images/icons/arrow.png"
 import backpack from "/home/friedrichtenhagen/ecommerce-site/src/images/backpack.jpg"
-
+import colors from "/home/friedrichtenhagen/ecommerce-site/src/images/colors.webp"
 
 export default function Body(){
     return(
         <div className="body">
             <div className="block1">
-                <div className="marginLeft">
+                <div className="marginLeft1">
                     <div className="b1Titel">
                         <h2 className="closeH2">Up to 50% off</h2>
                         <h2 className="closeH2">Spring Sale</h2>
@@ -34,7 +34,20 @@ export default function Body(){
                     </div>
             </div>
             <div className="block2">
-                
+                <div className="marginLeft2">
+                    <div className="b2Titel">
+                        <h2 className="closeH2">Monochrome Collection</h2>
+                        <h2 className="closeH2">New Colors Are Here</h2>
+                    </div>
+                    <div className="b2Cta">
+                        <div className="b2CtaText">
+                            Shop now
+                        </div>
+                        <img src={arrow} id="ctaArrow" alt="arrow right" />
+                    </div>
+                    
+                </div>
+                <img src={colors} id="colors" alt="colors" />
             </div>
             <div className="block3">
                 
