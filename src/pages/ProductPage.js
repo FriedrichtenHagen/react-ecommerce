43bdcb9acea41cf9e37e56efab6bfec96a2be2dc
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-04-26 19:05:06 +0200

    add modal

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,17 +1,38 @@
+import { useState } from 'react';
 import Header from "../components/Header";
 import Footer from '../components/Footer';
 import { useParams } from "react-router-dom";
 import data from "../data/product-feed"
 import arrowDown from "/home/friedrichtenhagen/ecommerce-site/src/images/icons/arrowDown.png"
 
+
 export default function ProductPage(){
+    const [cartModalActive, setCartModalActive] = useState(false)
+
 
     // receive the product name from the router
     const { productName } = useParams();
     // find matching object in product feed
     let product = data.find(o => o.name===productName)
-    console.log(product)
-    // display information from product object
+
+
+    
+
+    // open the size menu
+    function handleSizeClick(){
+        console.log("marla")
+        setCartModalActive(prevState => !prevState)
+    }
+
+    // add item to cart
+    function handleCartClick(){
+        console.log(product)
+
+
+        // cart needs to be in state
+        // highest shared level of product page and header (display the number of cart items in the cart icon)
+
+    }
 
 
     return(
@@ -32,16 +53,26 @@ export default function ProductPage(){
                         </div>
                     </div>
                 </div>
-                <div className="pickSize">
-                
+
+
+
+                <div className="pickSize" onClick={handleSizeClick}>
                     <div className="size">XL</div>
                     <img src={arrowDown} alt="arrow" id="arrowDown" />
                 </div>
-                <div className="addToCart">
+
+
+                <div className="addToCart" onClick={handleCartClick}>
                     Add to cart
                 </div>
+
+
+
             </div>
             <Footer/>
+            <div className={"cartModal " + (cartModalActive ? "visible" : "hidden")}>
+                Lets party
+            </div>
         </div>
     )
 }    
