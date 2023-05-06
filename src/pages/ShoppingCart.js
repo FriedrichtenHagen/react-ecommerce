d5f69c3958fd595fc3ea7d25adcf45ddfea70fa4
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-05-06 21:32:28 +0200

    Add a total price display

--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -22,6 +22,17 @@ export default function ShoppingCart( {cart, handleRemovingItemFromCart, handleA
             />
         )
     })
+    let subtotal=0;
+    cart.forEach(product => {
+        subtotal = subtotal+product.amount*product.price
+    })
+    let delivery;
+    if(subtotal<=90){
+        delivery=4.99
+    } else{
+        delivery=0
+    }
+    let total = Math.round((subtotal+delivery) * 100) / 100
 
     return (
         <div className="shoppingCart">
@@ -30,6 +41,32 @@ export default function ShoppingCart( {cart, handleRemovingItemFromCart, handleA
                 Shopping Cart ({cart.length} products)
             </div>
             <div className="shoppingCards">{cartList}</div>
+            <div className="total">
+                <div className="shoppingCartHeader">
+                    Total
+                </div>
+                <div className="totalBody">
+                    <div className="subTotal">
+                        <div className="subTotalText">Subtotal</div>
+                        <div className="subTotalNumber">
+                            {subtotal}
+                        </div>
+                    </div>
+                    <div className="delivery">
+                        <div className="deliveryText">Delivery</div>
+                        <div className="deliveryNumber">
+                            {delivery}
+                        </div>
+                    </div>
+                    <div className="totalPrice">
+                        <div className="totalText">Total (VAT included)</div>
+                        <div className="totalNumber">
+                            {total}
+                        </div>
+                    </div>
+                    <button className="checkout">GO TO CHECKOUT</button>
+                </div>
+            </div>
             <Footer/>
         </div>
     );
