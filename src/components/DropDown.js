49e6742986516f65318040cf3ab220b5b71b17e6
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-05-11 18:45:24 +0200

    finish transitions on drop down coupon field

--- src/components/DropDown.js
+++ src/components/DropDown.js
@@ -8,15 +8,18 @@ export default function DropDown(){
     function handleDropDown(){
         setExpand(!expand)
     }
+    let expandClassName = `drop-down-expand `+ (expand ? null : `drop-down-expand-active`)
+    let expandIconActive = expand ? `expandIconActive` : null
 
     return(
         <div className="drop-down">
             <div className="drop-down-top">
                 <div className="drop-down-base">Add a voucher (Optional)</div>
-                <img src={dropdown} id="dropdownicon" alt="drop down arrow" onClick={handleDropDown}/>
+                <img src={dropdown} id="dropdownicon" className={expandIconActive} alt="drop down arrow" onClick={handleDropDown}/>
             </div>
-            <div className="drop-down-expand">
-                Surprise {expand}
+            <div className={expandClassName}>
+                <div className="enterCodeText">Enter or paste a discount code here</div>
+                <input type="text" id="codeInput" />
             </div>
         </div>
     )
