6feebcd803547e25673d878499ee48b45addc917
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-05-19 17:53:07 +0300

    Save state for search Term.

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -10,6 +10,7 @@ import close from "/home/friedrichtenhagen/ecommerce-site/src/images/icons/close
 
 export default function CategoryPage( {cart}){
     const [productFeed, setProductFeed] = useState(data)
+    const [searchResultState, setSearchResultState] = useState(null)
 
     const location = useLocation();
     let searchResults=null;
@@ -17,16 +18,23 @@ export default function CategoryPage( {cart}){
     if(location.state){
         searchResults = location.state.searchMatches
         searchText = location.state.searchTerm
+        //setSearchResultState(searchResults)
     }
     // if searchterm or productfeed is empty show a message
    
+
+    function handleSearchClose(){
+        location.state.searchMatches=null;
+        searchText=null;
+        console.log(location.state.searchMatches)
+    }
  
     return(
         <div className="categoryPage">
             <Header cart={cart}/>
                 <div className="categoryHeader">
                     {searchResults ? <div className="searchTerm">
-                        {searchText} <img src={close} id="searchTermClose" alt="" /></div> : null}
+                        {searchText} <img src={close} id="searchTermClose" alt="" onClick={handleSearchClose} /></div> : null}
                     
                     <div className="categoryHeaderTitle">Nalgene Bottles on SALE</div>
                     <ul className="categoryUl">
